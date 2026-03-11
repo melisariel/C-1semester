1fa1a76b68bfcc5218a67c4d4107294fb5f568b5
melisariel <waterproofed@bk.ru>
2026-03-11 21:35:48 +0300

    Add files via upload

---
+++ lab1.c
@@ -0,0 +1,22 @@
+﻿// Поиск значения функции F
+
+#include <stdio.h>
+
+int main() {
+    double x, F;
+
+    scanf("%lf", &x);
+
+    if (x >= 0 && x <= 3) {
+        F = x * x - 3 * x + 9;
+        printf("F1 = %.2f\n", F);
+
+        F = 1 / (x * x * x + 6);
+        printf("F = %.2f\n", F);
+    }
+    else {
+        printf("Ошибка\n");
+    }
+
+    return 0;
+}
