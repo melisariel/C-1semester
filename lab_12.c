5a53d59bec939e313fa596a9ecf6ebf192286602
melisariel <waterproofed@bk.ru>
2026-03-12 11:09:13 +0300

    Add files via upload

---
+++ lab_12.c
@@ -0,0 +1,18 @@
+// Сумма цифр числа
+
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0;
+    scanf("%d", &n);
+
+    if (n < 0) n = -n;
+
+    while (n > 0) {
+        sum += n % 10;
+        n = n / 10;
+    }
+    
+    printf("%d\n", sum);
+    return 0;
+}
